7db74c26192f1eee4a919478f76d1718b678a380
alpaca-tc <alpaca-tc@alpaca.tc>
2023-01-04 19:09:44 +0900

    Support git_status

---
+++ denops/@ddu-kinds/git_status.ts
@@ -0,0 +1,137 @@
+import {
+  ActionFlags,
+  Actions,
+  BaseKind,
+  DduItem,
+  Item,
+} from "https://deno.land/x/ddu_vim@v2.0.0/types.ts";
+
+import { Denops } from "https://deno.land/x/ddu_vim@v2.0.0/deps.ts";
+
+export type StatusSymbol = " " | "M" | "A" | "D" | "R" | "C" | "U" | "?";
+
+export const StatusSymbols: { [key in StatusSymbol]: string } = {
+  " ": "Unmodified",
+  "M": "Modified",
+  "A": "Added",
+  "D": "Deleted",
+  "R": "Renamed",
+  "C": "Copied",
+  "U": "Unmerged",
+  "?": "Untracked",
+};
+
+export type Params = {
+  gitCommand: string;
+  openCommand: "edit";
+};
+
+export type ActionData = {
+  fullpath: string;
+  workingTree: StatusSymbol;
+  index: StatusSymbol;
+};
+
+const runCommand = async (cmd: string[], denops: Denops): Promise<void> => {
+  try {
+    const p = Deno.run({
+      cmd,
+      stdout: "piped",
+      stderr: "piped",
+      stdin: "piped",
+    });
+
+    await p.status();
+  } catch (e) {
+    await denops.call(
+      "ddu#util#print_error",
+      `Run ${cmd} is failed.`,
+    );
+
+    if (e instanceof Error) {
+      await denops.call(
+        "ddu#util#print_error",
+        e.message,
+      );
+    }
+  }
+};
+
+export class Kind extends BaseKind<Params> {
+  override actions: Actions<Params> = {
+    open: async (
+      args: { sourceParams: Params; denops: Denops; items: DduItem[] },
+    ) => {
+      const openCommand = args.sourceParams.openCommand ?? "edit";
+      const items = args.items as (DduItem & Item<ActionData>)[];
+
+      for (const item of items) {
+        await args.denops.call(
+          "ddu#util#execute_path",
+          openCommand,
+          item.action!.fullpath,
+        );
+      }
+
+      return await Promise.resolve(ActionFlags.Persist);
+    },
+
+    add: async (
+      args: { sourceParams: Params; denops: Denops; items: DduItem[] },
+    ) => {
+      const items = args.items as (DduItem & Item<ActionData>)[];
+
+      const cmd: string[] = [
+        args.sourceParams.gitCommand,
+        "add",
+      ];
+
+      items.forEach((item) => cmd.push(item.action!.fullpath));
+
+      await runCommand(cmd, args.denops);
+      return await Promise.resolve(ActionFlags.RefreshItems);
+    },
+
+    unstage: async (
+      args: { sourceParams: Params; denops: Denops; items: DduItem[] },
+    ) => {
+      const items = args.items as (DduItem & Item<ActionData>)[];
+
+      const cmd: string[] = [
+        args.sourceParams.gitCommand,
+        "reset",
+        "HEAD",
+      ];
+
+      items.forEach((item) => cmd.push(item.action!.fullpath));
+
+      await runCommand(cmd, args.denops);
+
+      return await Promise.resolve(ActionFlags.RefreshItems);
+    },
+
+    restore: async (
+      args: { sourceParams: Params; denops: Denops; items: DduItem[] },
+    ) => {
+      const items = args.items as (DduItem & Item<ActionData>)[];
+
+      const cmd: string[] = [
+        args.sourceParams.gitCommand,
+        "restore",
+      ];
+
+      items.forEach((item) => cmd.push(item.action!.fullpath));
+
+      await runCommand(cmd, args.denops);
+
+      return await Promise.resolve(ActionFlags.RefreshItems);
+    },
+  };
+
+  override params(): Params {
+    return {
+      gitCommand: "git",
+      openCommand: "edit",
+    };
+  }
+}
---
+++ denops/@ddu-sources/git_status.ts
@@ -0,0 +1,211 @@
+import {
+  BaseSource,
+  Item,
+  SourceOptions,
+} from "https://deno.land/x/ddu_vim@v2.0.0/types.ts";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.0.0/deps.ts";
+import { abortable } from "https://deno.land/std@0.165.0/async/mod.ts";
+import { ActionData } from "../@ddu-kinds/git_status.ts";
+import { Params as KindParams } from "../@ddu-kinds/git_branch.ts";
+import { TextLineStream } from "https://deno.land/std@0.165.0/streams/mod.ts";
+import { StatusSymbol, StatusSymbols } from "../@ddu-kinds/git_status.ts";
+import { join } from "https://deno.land/std@0.159.0/path/mod.ts";
+import { sprintf } from "https://deno.land/std@0.41.0/fmt/sprintf.ts";
+
+type Params = KindParams & {
+  path: string;
+};
+
+async function* iterLine(r: Deno.Reader): AsyncIterable<string> {
+  const lines = r.readable
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(new TextLineStream());
+
+  for await (const line of lines) {
+    if (line.length) {
+      yield line;
+    }
+  }
+}
+
+const STATUS_RE = /^(.)(.)\s(.*)$/;
+
+const resolveStatusSymbol = (maybeSymbol: string): StatusSymbol => {
+  if (maybeSymbol in StatusSymbols) {
+    return maybeSymbol as StatusSymbol;
+  } else {
+    return " " as StatusSymbol;
+  }
+};
+
+export class Source extends BaseSource<Params> {
+  override kind = "git_status";
+
+  override gather(args: {
+    denops: Denops;
+    sourceOptions: SourceOptions;
+    sourceParams: Params;
+    input: string;
+  }): ReadableStream<Item<ActionData>[]> {
+    const abortController = new AbortController();
+
+    const parseLine = (line: string, cwd: string): Item<ActionData> => {
+      line.trim();
+      const match = line.match(STATUS_RE);
+
+      if (match) {
+        const index = resolveStatusSymbol(match[1]);
+        const workingTree = resolveStatusSymbol(match[2]);
+        const relativePath = String(match[3]);
+        const word = sprintf(
+          "%-12s%-12s %s",
+          `[${StatusSymbols[index]}]`,
+          `<${StatusSymbols[workingTree]}>`,
+          relativePath,
+        );
+
+        return {
+          word,
+          action: {
+            index,
+            workingTree,
+            fullpath: join(cwd, relativePath),
+          },
+        };
+      } else {
+        throw new Error("Failed to parse git status");
+      }
+    };
+
+    const getRootDir = async (cwd: string): Promise<string | undefined> => {
+      return await this.getRootDir(
+        cwd,
+        args.sourceParams.gitCommand,
+        abortController,
+      );
+    };
+
+    return new ReadableStream({
+      async start(controller) {
+        const cwd = args.sourceParams.path === ""
+          ? await fn.getcwd(args.denops) as string
+          : args.sourceParams.path;
+
+        const rootDir = await getRootDir(cwd);
+
+        const items: Item<ActionData>[] = [];
+
+        if (!rootDir) {
+          // not git directory
+          controller.enqueue(items);
+          return;
+        }
+
+        const cmd = [
+          args.sourceParams.gitCommand,
+          "status",
+          "--porcelain=v1",
+        ];
+
+        const proc = Deno.run({
+          cmd: cmd,
+          stdout: "piped",
+          stderr: "piped",
+          stdin: "null",
+          cwd: cwd,
+        });
+
+        try {
+          for await (
+            const line of abortable(
+              iterLine(proc.stdout),
+              abortController.signal,
+            )
+          ) {
+            items.push(parseLine(line, rootDir));
+          }
+          if (items.length) {
+            controller.enqueue(items);
+          }
+        } catch (e: unknown) {
+          if (e instanceof DOMException) {
+            proc.kill("SIGTERM");
+          } else {
+            console.error(e);
+          }
+        } finally {
+          const [status, stderr] = await Promise.all([
+            proc.status(),
+            proc.stderrOutput(),
+          ]);
+          proc.close();
+          if (!status.success) {
+            const mes = new TextDecoder().decode(stderr);
+            console.error(mes);
+          }
+          controller.close();
+        }
+      },
+
+      cancel(reason): void {
+        abortController.abort(reason);
+      },
+    });
+  }
+
+  override params(): Params {
+    return {
+      gitCommand: "git",
+      path: "",
+    };
+  }
+
+  private async getRootDir(
+    cwd: string,
+    gitCommand: string,
+    abortController: AbortController,
+  ): Promise<string | undefined> {
+    const cmd = [
+      gitCommand,
+      "rev-parse",
+      "--show-superproject-working-tree",
+      "--show-toplevel",
+    ];
+
+    const proc = Deno.run({
+      cmd: cmd,
+      stdout: "piped",
+      stderr: "piped",
+      stdin: "null",
+      cwd: cwd,
+    });
+
+    const lines: string[] = [];
+
+    for await (
+      const line of abortable(
+        iterLine(proc.stdout),
+        abortController.signal,
+      )
+    ) {
+      line.trim();
+      lines.push(line);
+    }
+
+    return lines[0];
+  }
+}
+
+// const exists = async (path: string) => {
+//   // Note: Deno.stat() may be failed
+//   try {
+//     const stat = await Deno.stat(path);
+//     if (stat.isDirectory || stat.isFile || stat.isSymlink) {
+//       return true;
+//     }
+//   } catch (_: unknown) {
+//     // Ignore stat exception
+//   }
+//
+//   return false;
+// };
